9096b7e1babf58f88f0cfda6cc084f69761bebbd
Riquelme Magalhaes <riquelme.souza@arapiraca.ufal.br>
2025-08-30 15:30:32 -0300

    feat: add colorized terminal output to hare race

--- hare.py
+++ hare.py
@@ -2,12 +2,14 @@ import random
 import time
 from values import MAX_REST_SECONDS, JUMP_MAX_DISTANCE
 
+
 class Hare:
     def __init__(self, _id):
         self.id = _id
         self.id_normalized = _id[:20] if len(_id) > 20 else _id.ljust(20)
         self.track_distance = 0
         self.jumps = 0
+        self.color = '\033[37m'
 
     def jump(self):
         distance = random.uniform(1, JUMP_MAX_DISTANCE)
@@ -19,4 +21,3 @@ class Hare:
         seconds = random.uniform(0, MAX_REST_SECONDS)
         time.sleep(seconds)
         return seconds
-
--- race.py
+++ race.py
@@ -11,6 +11,24 @@ from values import (
 )
 
 
+class Colors:
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    BRIGHT_RED = '\033[91m'
+    BRIGHT_GREEN = '\033[92m'
+    BRIGHT_YELLOW = '\033[93m'
+    BRIGHT_BLUE = '\033[94m'
+    BRIGHT_MAGENTA = '\033[95m'
+    BRIGHT_CYAN = '\033[96m'
+
+
 def main():
     countdown()
 
@@ -27,9 +45,19 @@ def main():
         4: 'Programador Web'
     }
 
-    hares, threads = create_hare_threads(hare_names, semaphore, ranking, ranking_lock, race_updated)
-    
-    monitor_thread = threading.Thread(target=monitor_race, args=(hares, race_updated))
+    hare_colors = {
+        0: Colors.BRIGHT_RED,
+        1: Colors.BRIGHT_BLUE,
+        2: Colors.BRIGHT_GREEN,
+        3: Colors.BRIGHT_MAGENTA,
+        4: Colors.BRIGHT_YELLOW
+    }
+
+    hares, threads = create_hare_threads(
+        hare_names, hare_colors, semaphore, ranking, ranking_lock, race_updated)
+
+    monitor_thread = threading.Thread(
+        target=monitor_race, args=(hares, race_updated))
     monitor_thread.start()
 
     start_and_join_threads(threads)
@@ -39,19 +67,19 @@ def main():
 
 
 def countdown():
-    print(r'''
+    print(f'''{Colors.BRIGHT_CYAN}
  _   _    _    ____  _____   ____      _    ____ _____ 
 | | | |  / \  |  _ \| ____| |  _ \    / \  / ___| ____|
 | |_| | / _ \ | |_) |  _|   | |_) |  / _ \| |   |  _|  
 |  _  |/ ___ \|  _ <| |___  |  _ <  / ___ \ |___| |___ 
 |_| |_/_/   \_\_| \_\_____| |_| \_\/_/   \_\____|_____|
-    ''')
+{Colors.RESET}''')
 
     for i in range(3, 0, -1):
-        print(i, end='... ')
+        print(f'{Colors.BRIGHT_YELLOW}{i}{Colors.RESET}', end='... ')
         sys.stdout.flush()
         time.sleep(1)
-    print('GO!')
+    print(f'{Colors.BRIGHT_GREEN}{Colors.BOLD}GO!{Colors.RESET}')
 
 
 def hare_behaviour(hare: Hare, semaphore: Semaphore, ranking: list, ranking_lock: threading.Lock, race_updated: threading.Condition):
@@ -74,13 +102,15 @@ def hare_behaviour(hare: Hare, semaphore: Semaphore, ranking: list, ranking_lock
         hare.rest()
 
 
-def create_hare_threads(hare_names, semaphore, ranking, ranking_lock, race_updated):
+def create_hare_threads(hare_names, hare_colors, semaphore, ranking, ranking_lock, race_updated):
     hares = []
     threads = []
     for i in range(HARE_NUMBER):
         hare = Hare(hare_names.get(i, '???'))
+        hare.color = hare_colors.get(i, Colors.WHITE)
         hares.append(hare)
-        t = threading.Thread(target=hare_behaviour, args=(hare, semaphore, ranking, ranking_lock, race_updated))
+        t = threading.Thread(target=hare_behaviour, args=(
+            hare, semaphore, ranking, ranking_lock, race_updated))
         threads.append(t)
     return hares, threads
 
@@ -93,20 +123,31 @@ def start_and_join_threads(threads):
 
 
 def display_ranking(ranking):
+    print(f'\n{Colors.BOLD}{Colors.BRIGHT_CYAN}🏆 RANKING FINAL 🏆{Colors.RESET}\n')
     position_suffixes = {1: 'st', 2: 'nd', 3: 'rd'}
+    medal_colors = {1: Colors.BRIGHT_YELLOW, 2: Colors.WHITE, 3: Colors.YELLOW}
+    medals = {1: '🥇', 2: '🥈', 3: '🥉'}
+
     for position, hare in enumerate(ranking, start=1):
         suffix = position_suffixes.get(position, 'th')
-        print(f'{position}{suffix} - {hare.id} ({hare.jumps} jumps)')
+        medal = medals.get(position, '🏃')
+        color = medal_colors.get(position, Colors.WHITE)
+        print(f'{medal} {color}{Colors.BOLD}{position}{suffix}{Colors.RESET} - {hare.color}{hare.id}{Colors.RESET} ({Colors.CYAN}{hare.jumps} jumps{Colors.RESET})')
 
 
 def print_race_state(hares: list[Hare]):
     os.system('cls' if os.name == 'nt' else 'clear')
+    print(f'{Colors.BOLD}{Colors.BRIGHT_CYAN}🐇 CORRIDA DE LEBRES 🐇{Colors.RESET}\n')
+
     state = ''
     for hare in hares:
         dist = int(hare.track_distance)
         dist = RACE_DISTANCE if dist > RACE_DISTANCE else dist
         dist_left = RACE_DISTANCE - dist
-        state += f'{hare.id_normalized} - {"#" * dist}{" " * dist_left}|\n'
+
+        progress_bar = f"{hare.color}{'█' * dist}{Colors.WHITE}{'░' * dist_left}{Colors.RESET}"
+        state += f'{hare.color}{hare.id_normalized}{Colors.RESET} - {progress_bar}|{Colors.BRIGHT_GREEN}🏁{Colors.RESET}\n'
+
     print(state)
 
 
